fd8ea0499e6f4a011dc42a21bf09536a01f5fafb
Sergey Uskov <shidoshiho@gmail.com>
2019-11-25 00:07:51 +0300

    bit counting codewars solution

---
+++ bit-wise/bit-counting.js
@@ -0,0 +1,5 @@
+// Bit counting programming codewars
+
+// precondition : receive positive integer
+// postcondition: return number of bits equal to 1
+const bitCounting = n.toString(2).split("0").join("").length
