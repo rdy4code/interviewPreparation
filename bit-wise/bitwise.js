e1e1178724696dc2a7bada6e933bdc417df198fa
Sergey Uskov <shidoshiho@gmail.com>
2019-11-24 15:09:43 +0300

    biwise logic examples

--- bit-wise/bitwise.js
+++ bit-wise/bitwise.js
@@ -12,4 +12,12 @@ console.log(1 | 2); // --> 3
 //  if both bits are 1 then result bit will be 1
 console.log(1 & 2); // --> 0
 
+// What is logic behind ???
+// If the user has permission for some actions
+// for example Read , Write , Execute
+// 00000100 -Read
+// 00000010 - Write
+// 00000001 - Execute
+// 00000111  - All actions
+
 
