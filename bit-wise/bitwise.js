3bf8f559393745047bfe3f7d1daf4eaa15a4907e
Sergey Uskov <shidoshiho@gmail.com>
2019-11-24 14:58:29 +0300

    bitwise AND

--- bit-wise/bitwise.js
+++ bit-wise/bitwise.js
@@ -1,5 +1,15 @@
-// bitwise operator OR logic
+// bitwise operator OR
 //          1 - 00000001
 //          2 - 00000010
-// result       00000011 
+// result       00000011
+// if one of the bits 1, result's is 1 
 console.log(1 | 2); // --> 3
+
+// bitwise AND
+//          1 - 00000001
+//          2 - 00000010
+// result       00000000
+//  if both bits are 1 then result bit will be 1
+console.log(1 & 2); // --> 0
+
+
